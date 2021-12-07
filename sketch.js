f69ed5f6c4d75c00df26f455049558fbf32774e5
BeHappppy <42983254+BeHappppy@users.noreply.github.com>
2021-12-07 11:29:23 -0500

    ability to change control points 1 & 4
    added a way to change control points 1 & 4, same buttons as control points 2 & 3 however. so added a way to switch between the 2 by pressing the space bar

--- sketch.js
+++ sketch.js
@@ -1,6 +1,7 @@
 var px = [0, 100, 400, 600]; //control points x pos
 var py = [300, 100, 400, 300]; //control points y pos
 var speed = 5; //how fast the points move
+var pointSwitch = 1; //wich points will move? (control points 1 & 4 or 2 & 3)
 
 function setup() {
   createCanvas(600, 600);
@@ -9,37 +10,77 @@ function setup() {
 function draw() {
   background(0);
   
-  //drawing the bezier curve
-  theCurve();
-}
-
-function theCurve () {
-  //changing the 2nd control point
-  if(keyIsDown(65)) {
-     px[1] -= speed;
-  } // 65 = "a"
-  else if (keyIsDown(68)) {
-    px[1] += speed;
-  } // 68 = "d"
-  else if (keyIsDown(87)) {
-    py[1] -= speed;
-  } // 87 = "w"
-  else if (keyIsDown(83)) {
-    py[1] += speed;
-  } // 83 = "s"
+  //change which points are changing with the space bar
+  if(keyIsDown(32)) {
+    if (pointSwitch === 1) {
+      pointSwitch = 0;
+    }
+    else {
+      pointSwitch = 1;
+    }
+  }
   
-  //changing the 3rd control point
-  if(keyIsDown(LEFT_ARROW)) {
-    px[2] -= speed;
-  }
-  else if(keyIsDown(RIGHT_ARROW)) {
-    px[2] += speed;
-  }
-  else if(keyIsDown(UP_ARROW)) {
-    py[2] -= speed;
-  }
-  else if(keyIsDown(DOWN_ARROW)) {
-    py[2] += speed;
+  //change what pressing WASD or the arrow keys do (change control points 1 & 4 or 2 & 3)
+  switch(pointSwitch) {
+    case 0:
+      //changing the 2nd control point
+      if(keyIsDown(65)) {
+         px[0] -= speed;
+      } // 65 = "a"
+      else if (keyIsDown(68)) {
+        px[0] += speed;
+      } // 68 = "d"
+      else if (keyIsDown(87)) {
+        py[0] -= speed;
+      } // 87 = "w"
+      else if (keyIsDown(83)) {
+        py[0] += speed;
+      } // 83 = "s"
+
+      //changing the 3rd control point
+      if(keyIsDown(LEFT_ARROW)) {
+        px[3] -= speed;
+      }
+      else if(keyIsDown(RIGHT_ARROW)) {
+        px[3] += speed;
+      }
+      else if(keyIsDown(UP_ARROW)) {
+        py[3] -= speed;
+      }
+      else if(keyIsDown(DOWN_ARROW)) {
+        py[3] += speed;
+      }
+      break;
+      
+    case 1:
+      //changing the 2nd control point
+      if(keyIsDown(65)) {
+         px[1] -= speed;
+      } // 65 = "a"
+      else if (keyIsDown(68)) {
+        px[1] += speed;
+      } // 68 = "d"
+      else if (keyIsDown(87)) {
+        py[1] -= speed;
+      } // 87 = "w"
+      else if (keyIsDown(83)) {
+        py[1] += speed;
+      } // 83 = "s"
+
+      //changing the 3rd control point
+      if(keyIsDown(LEFT_ARROW)) {
+        px[2] -= speed;
+      }
+      else if(keyIsDown(RIGHT_ARROW)) {
+        px[2] += speed;
+      }
+      else if(keyIsDown(UP_ARROW)) {
+        py[2] -= speed;
+      }
+      else if(keyIsDown(DOWN_ARROW)) {
+        py[2] += speed;
+      }
+      break;
   }
   
   //control points
@@ -60,5 +101,4 @@ function theCurve () {
   strokeWeight(4);
   bezier(px[0], py[0], px[1], py[1], px[2], py[2], px[3], py[3]);
 }
-
 
