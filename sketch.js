6233121e26f79cd5d6905b8abcd6462d365edce6
BeHappppy <42983254+BeHappppy@users.noreply.github.com>
2021-12-07 10:53:24 -0500

    put variables about control points into array
    put the old variables that control the control points into an array for better access, also put the first and fourth ones in there as well

--- sketch.js
+++ sketch.js
@@ -1,9 +1,6 @@
-var p2x = 100; //control point 2 x pos
-var p2y = 100; //control point 2 y pos
-var p3x = 400; //control point 3 x pos
-var p3y = 400; //control point 3 y pos
+var px = [0, 100, 400, 600]; //control points x pos
+var py = [300, 100, 400, 300]; //control points y pos
 var speed = 5; //how fast the points move
-
 
 function setup() {
   createCanvas(600, 600);
@@ -12,57 +9,56 @@ function setup() {
 function draw() {
   background(0);
   
-  //Drawing the basic bezier curve
-  basicCurve();
+  //drawing the bezier curve
+  theCurve();
 }
 
-function basicCurve () {
+function theCurve () {
   //changing the 2nd control point
   if(keyIsDown(65)) {
-     p2x -= speed;
+     px[1] -= speed;
   } // 65 = "a"
   else if (keyIsDown(68)) {
-    p2x += speed;
+    px[1] += speed;
   } // 68 = "d"
   else if (keyIsDown(87)) {
-    p2y -= speed;
+    py[1] -= speed;
   } // 87 = "w"
   else if (keyIsDown(83)) {
-    p2y += speed;
+    py[1] += speed;
   } // 83 = "s"
   
   //changing the 3rd control point
   if(keyIsDown(LEFT_ARROW)) {
-    p3x -= speed;
+    px[2] -= speed;
   }
   else if(keyIsDown(RIGHT_ARROW)) {
-    p3x += speed;
+    px[2] += speed;
   }
   else if(keyIsDown(UP_ARROW)) {
-    p3y -= speed;
+    py[2] -= speed;
   }
   else if(keyIsDown(DOWN_ARROW)) {
-    p3y += speed;
+    py[2] += speed;
   }
   
   //control points
   stroke(255);
   strokeWeight(20);
-  point(0,300);
-  point(p2x, p2y);
-  point(p3x, p3y);
-  point(600, 300);
+  point(px[0], py[0]);
+  point(px[1], py[1]);
+  point(px[2], py[2]);
+  point(px[3], py[3]);
   
   //lines
   strokeWeight(2);
-  line(0, 300, p2x, p2y)
-  line(p3x, p3y, 600, 300);
+  line(px[0], py[0], px[1], py[1])
+  line(px[2], py[2], px[3], py[3]);
   
   //the bezier curve
   noFill();
   strokeWeight(4);
-  bezier(0, 300, p2x, p2y, p3x, p3y, 600, 300);
+  bezier(px[0], py[0], px[1], py[1], px[2], py[2], px[3], py[3]);
 }
-
 
 
