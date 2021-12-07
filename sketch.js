27db4c5a84e7cd9fa7c4afaee826ef652d658775
BeHappppy <42983254+BeHappppy@users.noreply.github.com>
2021-12-07 10:30:28 -0500

    Update sketch.js
    Put the Bezier curve into a function

--- sketch.js
+++ sketch.js
@@ -5,6 +5,13 @@ var p3y = 400; //control point 3 y pos
 var speed = 5; //how fast the points move
 
 
+var p2x = 100; //control point 2 x pos
+var p2y = 100; //control point 2 y pos
+var p3x = 400; //control point 3 x pos
+var p3y = 400; //control point 3 y pos
+var speed = 5; //how fast the points move
+
+
 function setup() {
   createCanvas(600, 600);
 }
@@ -12,6 +19,11 @@ function setup() {
 function draw() {
   background(0);
   
+  //Drawing the basic bezier curve
+  basicCurve();
+}
+
+function basicCurve () {
   //changing the 2nd control point
   if(keyIsDown(65)) {
      p2x -= speed;
@@ -42,7 +54,7 @@ function draw() {
   
   //control points
   stroke(255);
-  strokeWeight(24);
+  strokeWeight(20);
   point(0,300);
   point(p2x, p2y);
   point(p3x, p3y);
@@ -58,3 +70,6 @@ function draw() {
   strokeWeight(4);
   bezier(0, 300, p2x, p2y, p3x, p3y, 600, 300);
 }
+
+
+
